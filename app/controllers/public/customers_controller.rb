ab81a6dac6a6c8e4f3b2f9be474c7bb372275089
imaaa34 <oct.imd34@gmail.com>
2021-06-19 05:02:11 +0000

    [Add] 会員退会機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,10 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @customer = current_customer
   end
-  
+
   def edit
     @customer = current_customer
   end
@@ -17,12 +17,18 @@ class Public::CustomersController < ApplicationController
       render :edit
     end
   end
-  
-  
-  
+
+  def withdraw
+    customer = current_customer
+    customer.update(is_valid: false)
+    reset_session
+    redirect_to root_path
+  end
+
+
   private
     def customer_params
       params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
     end
-      
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,36 @@
 Rails.application.routes.draw do
   devise_for :customers
-  
+
   #会員側のルーティング
   scope module: :public do
     root to: "homes#top"
     get 'about' => 'homes#about'
-    
-    resources :customers,only: [:edit,:update,:show] 
-    
+
+    resources :customers,only: [:edit, :update, :show] do
+      collection do
+        get 'unsubscribe'
+        patch 'withdraw'
+      end
+    end
+
     resources :products, only: [:index, :show]
-    
-    
+
+
     resources :cart_products, only: [:index, :create, :update]
       delete 'cart_products/destroy_all' => 'cart_products#destroy_all'
       delete 'cart_products/:id' => 'cart_products#destroy'
-      
+
     resources :orders, only: [:index, :show, :create, :new]
-    
-    
+
+
     resources :shipping_addresses, only: [:index, :create, :edit, :update, :destroy]
   end
-  
+
   #管理者側のルーティング
   devise_for :admins, controllers: {
   sessions: 'admins/sessions'
   }
-  
+
   namespace :admin do
     resources :products
   end
