ac01a14b5c649a56f4b3a4b692ee65c374888cf0
seikin123 <abc521fjr7@gmail.com>
2021-06-21 10:20:13 +0000

    [add]注文入力画面途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,6 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+  
   
   def index
   end
@@ -7,15 +9,26 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to root_path
   end
 
   def confirm
   end
 
   def new
+    @order = Order.new
+    @addresses = ShippingAddress.where(customer: current_customer)
   end
 
   def complete
+  end
+  
+  private
+  
+  def order_params
+    params.require(:oreder).permit(:postcode, :address, :name, :shipping, :total_price, :payment, :order_status)
   end
   
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
 
   has_many :products, through: :order_products
+  
+  belongs_to :customer
 
 end
