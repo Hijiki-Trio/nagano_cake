62758d94e298ce1d60d7d61a4f7779338ae92408
seikin123 <abc521fjr7@gmail.com>
2021-06-24 06:32:50 +0000

    [finish]注文履歴

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -4,12 +4,18 @@ class Admin::OrderProductsController < ApplicationController
   def update #製作ステータスの更新
 		@order_product = OrderProduct.find(params[:id])
 		@order = @order_product.order
-		@order_product.update(order_product_params)
-		redirect_to admin_order_path(@order)
+		if @order.order_status != "入金待ち"
+  		@order_product.update(order_product_params)
+  		@order_product.order_status_auto_update
+  		@order_product.product_auto_update
+  		redirect_to admin_order_path(@order)
+		else
+		  redirect_to admin_order_path(@order)
+		end
   end
   
   
-private
+ private
   def order_product_params
      params.require(:order_product).permit(:product_status)
   end
