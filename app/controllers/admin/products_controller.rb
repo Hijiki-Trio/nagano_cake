dbc2a5cb2f83425b1bf4e4f4bcaa87ddd2064976
imaaa34 <oct.imd34@gmail.com>
2021-06-25 02:32:00 +0000

    [Fix] pagenation抜けているところ追加

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -11,12 +11,12 @@ class Admin::ProductsController < ApplicationController
      render 'new'
     end
   end
-  
+
   def new
     @product = Product.new
     @genres = Genre.all
   end
-    
+
   def index
     @products = Product.page(params[:page]).per(10)
     @genres = Genre.all
@@ -27,12 +27,12 @@ class Admin::ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @genres = Genre.all
   end
-  
+
   def edit
     @product = Product.find(params[:id])
     @genres = Genre.all
   end
-  
+
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
@@ -41,10 +41,10 @@ class Admin::ProductsController < ApplicationController
      render :edit
     end
   end
-  
-  
+
+
  private
- 
+
   def product_params
     params.require(:product).permit(:name, :detail, :image, :price, :genre_id, :is_active)
   end
