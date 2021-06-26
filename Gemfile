b75431538336c00d3456a79a542dff8caa362553
seikin123 <abc521fjr7@gmail.com>
2021-06-26 02:40:19 +0000

    [add]テスト

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ group :test do
   gem 'rspec-rails'
   gem 'factory_bot_rails'
   gem 'database_cleaner'
+  
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,40 +1,65 @@
 require 'rails_helper'
 
-RSpec.feature "管理者機能", type: :feature do
+RSpec.feature "管理者 マスタ登録", type: :feature do
   before do
     @admin = FactoryBot.create(:admin)
     FactoryBot.create(:product)
   end
   
-  scenario "管理者が商品を追加することができる" do
-    #ログイン
-    visit new_admin_session_path
-    fill_in 'admin_email',    with: 'test@test.com'
-    fill_in 'admin_password', with: 'testtest'
-    first(:link, 'ログイン').click
-    #管理者用トップページへ遷移
-    expect(current_path).to eq new_customer_session_path
-
-    #ヘッダーの商品一覧リンクをクリック
-    click_link "商品一覧"
-    expect(current_path).to eq new_admin_product_path
-
-    ##商品登録
-    fill_in 'product_name', with: "商品名"
-    click_on "新規登録"
-    #追加した商品が表示されている
-    expect(page).to have_content '商品１'
+  context 'ログイン画面' do
+    it 'ログイン画面が表示されること' do
+      visit new_admin_session_path
+      expect(page).to have_content('メールアドレス')
+      expect(page).to have_content('パスワード')
+    end
   end
-
-  # scenario '管理者ユーザーがログアウトできる' do
-  #   visit new_admin_session_path
-  #   fill_in  'admin_email',    with: 'test@test.com'
-  #   fill_in  'admin_password', with: 'testtest'
-  #   click_on 'Log in'
-
-    ###（想定外）ログアウトリンクがない
-  #   click_on 'ログアウト'
-  #   expect(current_path).to new_admin_session_path
-  # end
+  
+  context 'フォームの入力値が正常' do
+    it 'ログインに成功' do
+      visit new_admin_session_path
+      fill_in 'admin_email', with: @admin.email
+      fill_in 'admin_password', with: @admin.password
+      click_button "ログイン"
+      expect(page)
+    end
+  
+     it "ログイン後注文履歴一覧に遷移" do
+      visit new_admin_session_path
+      fill_in 'admin_email', with: @admin.email
+      fill_in 'admin_password', with: @admin.password
+      click_button "ログイン"
+      expect(current_path).to eq admin_orders_path
+    end
+  end
+  
+  context 'メールアドレスが未入力' do
+    it 'ログインに失敗' do
+      visit new_admin_session_path
+      fill_in 'admin_email', with: ''
+      fill_in 'admin_password', with: @admin.password
+      click_button "ログイン"
+      expect(page).to have_content('Email もしくはパスワードが不正です。')
+    end
+  end
+  
+  context 'パスワードが未入力' do
+    it 'ログインに失敗' do
+      visit new_admin_session_path
+      fill_in 'admin_email', with: @admin.email
+      fill_in 'admin_password', with: ''
+      click_button "ログイン"
+      expect(page).to have_content('Email もしくはパスワードが不正です。')
+    end
+  end
+  
+  describe 'ログイン後' do
+    before do
+      visit new_admin_session_path
+      fill_in 'admin_email', with: @admin.email
+      fill_in 'admin_password', with: @admin.password
+      click_button "ログイン"
+      Genre.create(id:1, name: 'ケーキ')
+      @product = Product.create(id: 1,name: 'アンパン', genre_id: 1, price: 500, detail: 'うまい', is_active: true)
+    end
+  end
 end
-
